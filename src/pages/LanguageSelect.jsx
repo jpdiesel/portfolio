27634be99ea7284dee906b43c91e1d2252bdd56b
Diesel <jpedro.diesel@gmail.com>
2023-09-29 22:06:34 -0300

    add button and language radios styles

--- src/pages/LanguageSelect.jsx
+++ src/pages/LanguageSelect.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router';
-import '../styles/pages/LanguageSelect.css'
+import '../styles/pages/LanguageSelect.css';
+import brazilFlag from '../img/brazilFlag.png';
+import usaFlag from '../img/usaFlag.jpg';
 
 export default class LanguageSelect extends Component {
   constructor() {
@@ -10,6 +12,8 @@ export default class LanguageSelect extends Component {
       checkEN: false,
       redirectToPT: false,
       redirectToEN: false,
+      styledInputPT: false,
+      styledInputEN: false,
     }
     this.checkValidation = this.checkValidation.bind(this);
     this.redirectFunction = this.redirectFunction.bind(this);
@@ -21,11 +25,15 @@ export default class LanguageSelect extends Component {
       this.setState({
         checkEN: false,
         checkPT: true,
+        styledInputEN: false,
+        styledInputPT: true,
       })
     } if (language === "en") {
       this.setState({
         checkEN: true,
         checkPT: false,
+        styledInputEN: true,
+        styledInputPT: false,
       })
     }
   }
@@ -40,29 +48,44 @@ export default class LanguageSelect extends Component {
   }
 
   render() {
+    const StylesPT = {
+      border: "solid 2px #e6eaf0",
+      backgroundImage: `url(${brazilFlag})`,
+      backgroundSize: "cover",
+      color: "white",
+      
+    }
+    const StylesEN = {
+      border: "solid 2px #e6eaf0",
+      backgroundImage: `url(${usaFlag})`,
+      backgroundSize: "cover",
+      color: "white",
+    }
     const { 
       checkEN,
       checkPT,
       redirectToEN,
       redirectToPT,
+      styledInputEN,
+      styledInputPT,
      } = this.state;
     return (
       <div className="outside">
         <section className="board" id="language-board">
-          {checkPT ? <p className='pt-title'>Escolha seu idioma</p> : null }
-          {checkEN ? <p className='en-title'>Choose your language</p> : null}
-          {!checkEN && !checkPT ? <p className='pt-title'>Escolha seu idioma</p> : null}
-          <label className='en-label' htmlFor="en">
+          {checkPT ? <p className='pt-title'>ESCOLHA SEU IDIOMA</p> : null }
+          {checkEN ? <p className='en-title'>SELECT YOUR LANGUAGE</p> : null}
+          {!checkEN && !checkPT ? <p className='title'>ESCOLHA SEU IDIOMA</p> : null}
+          <label style={styledInputEN ? StylesEN : null} className='en-label' htmlFor="en">
             <input type="radio" id="en" name="language" onChange={ this.checkValidation }/>
             EN-US
           </label>
-          <label className='pt-label' htmlFor="pt">
+          <label style={styledInputPT ? StylesPT : null} className='pt-label' htmlFor="pt">
             <input type="radio" id="pt" name="language" onChange={ this.checkValidation }/>
             PT-BR
           </label>
-          <div className='space' />
-          {checkPT ? <button type="submit" id="pt-button" onClick={ this.redirectFunction }>Ir para página principal</button> : null}
-          {checkEN ? <button type="submit" id="en-button" onClick={ this.redirectFunction }>Proceed to main page</button> : null}
+          <div className='space'/>
+          {checkPT ? <button value="IR PARA PÁGINA PRINCIPAL" type="submit" id="pt-button" onClick={ this.redirectFunction }></button> : null}
+          {checkEN ? <button value="PROCEED TO MAIN PAGE" type="submit" id="en-button" onClick={ this.redirectFunction }></button> : null}
           {redirectToPT ? <Redirect to="/sobre"/> : null}
           {redirectToEN ? <Redirect to="/about"/> : null}
         </section>
