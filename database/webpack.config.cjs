dbeaefd59b3c5f81ac6b41120c86b115fe0c6428
João Pedro Diesel <jpedro.diesel@gmail.com>
2024-06-18 23:35:17 -0300

    add webpack

---
+++ database/webpack.config.cjs
@@ -0,0 +1,30 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    app: './App.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.cjs',
+    libraryTarget: 'commonjs2'
+  },
+  externals: [nodeExternals()],
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
